b838f9759575668fa72f79e5249c05626c3c1d3c
julivevo143s <vo000077@algonquinlive.com>
2023-07-24 16:28:14 -0400

    Week 1

--- app/src/main/java/MobileApplication/Group/Theme/AviationStackFlightTracker.java
+++ app/src/main/java/MobileApplication/Group/Theme/AviationStackFlightTracker.java
@@ -1,7 +1,12 @@
 package MobileApplication.Group.Theme;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
 import MobileApplication.Group.R;
 import MobileApplication.Group.databinding.ActivityFlightBinding;
 
@@ -10,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,167 +23,162 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import android.util.Log;
 
 
 public class AviationStackFlightTracker extends AppCompatActivity {
-    private static final String BASE_URL = "http://api.aviationstack.com/v1/flights";
-    private static final String ACCESS_KEY = "88047607053ddc44d85be397fed16b28";
-
-    private EditText airportCodeText;
-    protected ActivityFlightBinding binding;
-    private ChatRoomViewModel chatModel;
-    private ArrayList<FlightMessage> messages;
-    private FlightAdapter adapter;
+    private RequestQueue queue;
+    private List<String> airlineNames;
+    private RecyclerView recyclerView;
+    private FlightAdapter flightAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityFlightBinding.inflate(getLayoutInflater());
+
+        ActivityFlightBinding binding = ActivityFlightBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        chatModel = new ViewModelProvider(this).get(ChatRoomViewModel.class);
-        messages = chatModel.messages.getValue();
+        airlineNames = new ArrayList<>();
+        recyclerView = findViewById(R.id.flightRecyclerView);
+        flightAdapter = new FlightAdapter(airlineNames);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(flightAdapter);
 
-        if (messages == null) {
-            chatModel.messages.setValue(messages = new ArrayList<>());
-        }
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
 
-        airportCodeText = binding.textInput;
+        queue = new RequestQueue(cache, network);
+        queue.start();
 
-        binding.searchButton.setOnClickListener(v -> {
-            String airportCode = airportCodeText.getText().toString();
-            if (!airportCode.isEmpty()) {
-                String searchUrl = BASE_URL + "?access_key=" + ACCESS_KEY + "&dep_iata=" + airportCode;
-                new FlightSearchTask().execute(searchUrl);
-            }
+        binding.searchButton.setOnClickListener(click -> {
+            String airportCode = binding.flightSearch.getText().toString();
+
+            String url = "http://api.aviationstack.com/v1/flights?access_key=88047607053ddc44d85be397fed16b28&dep_iata=" +
+                    URLEncoder.encode(airportCode);
+
+            StringRequest request = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+
+//                            airlineNames.clear();
+//                            ExecutorService executor = Executors.newSingleThreadExecutor();
+//                            executor.execute(() -> {
+                            runOnUiThread(()->{
+//                                JSONArray flightDataArray = null;
+
+
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    JSONArray flightDataArray = jsonObject.getJSONArray("data");
+                                    int length = flightDataArray.length();
+
+                                    for (int i = 0; i < length; i++) {
+                                        JSONObject thisObj = flightDataArray.getJSONObject(i);
+                                        JSONObject airlineObject = thisObj.getJSONObject("airline");
+                                        String airlineName = airlineObject.getString("name");
+                                        int notimportant = 0;
+                                    }
+                                } catch (JSONException e) {
+                                    throw new RuntimeException(e);
+                                }
+
+//                                for (int i = 0; i < flightDataArray.length(); i++) {
+//                                    JSONObject flightData = flightDataArray.getJSONObject(i);
+//                                    JSONObject airlineObject = flightData.getJSONObject("airline");
+//                                    String airlineName = airlineObject.getString("name");
+//
+//                                    airlineNames.add(airlineName);
+//                                }
+//                                runOnUiThread(()->{
+//                                    airlineNames.clear();
+//                                    flightAdapter.notifyDataSetChanged();
+//                                });
+
+                            });
+
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // Handle error
+                    Log.e("API Error", "Error occurred during API request: " + error.getMessage());
+                    // error.printStackTrace();
+                }
+            });
+
+            queue.add(request);
         });
-
-        adapter = new FlightAdapter(messages);
-        binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
-        binding.recycleView.setAdapter(adapter);
-    }
-
-    private class FlightSearchTask extends AsyncTask<String, Void, List<FlightMessage>> {
-        @Override
-        protected List<FlightMessage> doInBackground(String... urls) {
-            String searchUrl = urls[0];
-            List<FlightMessage> flightList = new ArrayList<>();
-
-            try {
-                URL url = new URL(searchUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-
-                InputStream inputStream = connection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuilder response = new StringBuilder();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-
-                flightList = parseFlightData(response.toString());
-
-                reader.close();
-                inputStream.close();
-                connection.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return flightList;
-        }
-
-        @Override
-        protected void onPostExecute(List<FlightMessage> flightList) {
-            super.onPostExecute(flightList);
-            messages.clear();
-            messages.addAll(flightList);
-            adapter.notifyDataSetChanged();
-        }
-
-        private List<FlightMessage> parseFlightData(String jsonData) {
-            List<FlightMessage> flights = new ArrayList<>();
-
-            try {
-                JSONObject jsonObject = new JSONObject(jsonData);
-                JSONArray data = jsonObject.getJSONArray("data");
-                for (int i = 0; i < data.length(); i++) {
-                    JSONObject flightObject = data.getJSONObject(i);
-                    String destination = flightObject.getJSONObject("arrival").getString("iata");
-                    String terminal = flightObject.has("terminal") ? flightObject.getString("terminal") : "N/A";
-                    String gate = flightObject.has("gate") ? flightObject.getString("gate") : "N/A";
-                    String delay = flightObject.getJSONObject("departure").optString("delay", "");
-
-
-                    FlightMessage flight = new FlightMessage(destination, terminal, gate, delay);
-                    flights.add(flight);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            return flights;
-        }
     }
 
     private static class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.FlightViewHolder> {
-        private List<FlightMessage> flightList;
+        private List<String> airlineNames;
 
-        public FlightAdapter(List<FlightMessage> flights) {
-            flightList = flights;
+        public FlightAdapter(List<String> airlineNames) {
+            this.airlineNames = airlineNames;
         }
 
         @NonNull
         @Override
         public FlightViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            // Inflate the item layout for each row
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.seeflight_message, parent, false);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.flight_item_layout, parent, false);
             return new FlightViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull FlightViewHolder holder, int position) {
-            // Bind the flight data to the view holder
-            FlightMessage flight = flightList.get(position);
-            holder.destinationTextView.setText(flight.getDestination());
-            holder.terminalTextView.setText(flight.getTerminal());
-            holder.gateTextView.setText(flight.getGate());
-            holder.delayTextView.setText(flight.getDelay());
+            String airlineName = airlineNames.get(position);
+            holder.airlineNameTextView.setText(airlineName);
         }
 
         @Override
         public int getItemCount() {
-            return flightList.size();
+            return airlineNames.size();
         }
 
-        static class FlightViewHolder extends RecyclerView.ViewHolder {
-            TextView destinationTextView;
-            TextView terminalTextView;
-            TextView gateTextView;
-            TextView delayTextView;
+        public static class FlightViewHolder extends RecyclerView.ViewHolder {
+            TextView airlineNameTextView;
 
-            public FlightViewHolder(@NonNull View itemView) {
+            public FlightViewHolder(View itemView) {
                 super(itemView);
-                destinationTextView = itemView.findViewById(R.id.Destination);
-                terminalTextView = itemView.findViewById(R.id.Terminal);
-                gateTextView = itemView.findViewById(R.id.Gate);
-                delayTextView = itemView.findViewById(R.id.Delay);
+                airlineNameTextView = itemView.findViewById(R.id.airlineNameTextView);
             }
         }
     }
 }
+
