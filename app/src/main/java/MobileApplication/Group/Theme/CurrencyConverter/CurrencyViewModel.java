305809e0178fad7808f0c20eda0e61ed5854d50e
Dan Lyn <tiudanlyn@gmail.com>
2023-08-04 14:14:50 -0400

    CurrencyConverter
    CurrencyConverter
    
    CurrencyConverter
    
    CurrencyConverter
    
    CurrencyConverter
    
    CurrencyConverter
    
    CurrencyConverter
    
    CurrencyConverter

--- app/src/main/java/MobileApplication/Group/Theme/CurrencyConverter/CurrencyViewModel.java
+++ app/src/main/java/MobileApplication/Group/Theme/CurrencyConverter/CurrencyViewModel.java
@@ -2,16 +2,26 @@ package MobileApplication.Group.Theme.CurrencyConverter;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
 import java.util.ArrayList;
-
 import MobileApplication.Group.Theme.Data.ConversionHistory;
-import MobileApplication.Group.Theme.Spinner.CurrencySpinner;
 
 
+/**
+ * ViewModel class for managing currency conversions and selected conversion history entry.
+ * This class is responsible for holding and updating data related to currency conversions
+ * and the currently selected conversion history entry.
+ */
 public class CurrencyViewModel extends ViewModel {
 
-    public MutableLiveData<ArrayList<CurrencySpinner>> countrySpinnerList = new MutableLiveData< >();
-    public MutableLiveData<ArrayList<ConversionHistory>> conversionHistoryList = new MutableLiveData< >();
-
+    /**
+     * MutableLiveData holding the list of conversion history entries.
+     * Other classes can observe this LiveData to get notified of changes to the list of conversions.
+     */
+    public MutableLiveData<ArrayList<ConversionHistory>> conversions = new MutableLiveData< >();
+    /**
+     * MutableLiveData holding the currently selected conversion history entry.
+     * Other classes can observe this LiveData to get notified of changes to the selected conversion entry.
+     */
+    public MutableLiveData<ConversionHistory> selectedConversion = new MutableLiveData< >();
 }
+
--- app/src/main/java/MobileApplication/Group/Theme/Spinner/CurrencySpinner.java
+++ app/src/main/java/MobileApplication/Group/Theme/Spinner/CurrencySpinner.java
@@ -1,27 +1,56 @@
 package MobileApplication.Group.Theme.Spinner;
 
+/**
+ * The CurrencySpinner class represents a currency item with its country name and flag icon.
+ */
 public class CurrencySpinner {
 
+    /**
+     * The country name of the currency.
+     */
     String countryName;
+    /**
+     * The resource ID of the country's flag icon.
+     */
     int countryFlag;
-
+    /**
+     * Constructor for the CurrencySpinner class.
+     * @param countryName The name of the country associated with the currency.
+     * @param countryFlag The resource ID of the country's flag icon representing the currency.
+     */
     public CurrencySpinner(String countryName, int countryFlag){
         this.countryFlag = countryFlag;
         this.countryName = countryName;
     }
 
+    /**
+     * Get the resource ID of the country's flag icon.
+     * @return The resource ID of the country's flag icon.
+     */
     public int getCountryFlag() {
         return countryFlag;
     }
 
+    /**
+     * Set the resource ID of the country's flag icon.
+     * @param countryFlag The resource ID of the country's flag icon.
+     */
     public void setCountryFlag(int countryFlag) {
         this.countryFlag = countryFlag;
     }
 
+    /**
+     * Get the name of the country associated with the currency.
+     * @return The name of the country.
+     */
     public String getCountryName() {
         return countryName;
     }
 
+    /**
+     * Set the name of the country associated with the currency.
+     * @param countryName The name of the country.
+     */
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
